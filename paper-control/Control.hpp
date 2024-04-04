ab12721b0dfa4013f0aaeafdcb6f49350a2ab0c7
el-crag <azertyggmf@outlook.com>
2024-04-04 11:24:49 -0500

    Adding some attributes and methods about display in Control class

--- paper-control/Control.hpp
+++ paper-control/Control.hpp
@@ -1,16 +1,43 @@
 #ifndef CONTROL_INC
 #define CONTROL_INC
 
+#include "Queue.h"
+
 class Control {
 	protected:
 		static Control * control;
 		Control();
 
 	public:
+		enum DisplayStatus {
+			SENDING,
+			RECEIVING
+		};
+
 		// For singleton
 		static Control * getInstance();
 		Control(Control &other) = delete;
 		void operator=(const Control &) = delete;
+
+		/**
+		 * Queue as display instructions buffer.
+		 */
+		Queue<String> messagesQueue;
+
+		/**
+		 * Set instruction type for sending in display.
+		 */
+		void setDisplaySending();
+
+		/**
+		 * Set instruction type for receiving in display.
+		 */
+		void setDisplayReceiving();
+
+		DisplayStatus getDisplayStatus() const;
+
+	private:
+		DisplayStatus displayStatus;
 };
 
 #endif
