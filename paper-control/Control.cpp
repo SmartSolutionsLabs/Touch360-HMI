e153af98af5fb51068a5b9e86da237f4738570ac
el-crag <azertyggmf@outlook.com>
2024-04-04 14:26:44 -0500

    Fixing pin status in high or low states for sending and receiving

--- paper-control/Control.cpp
+++ paper-control/Control.cpp
@@ -15,10 +15,20 @@ Control::Control() : displayStatus(RECEIVING) {
 
 void Control::setDisplaySending() {
 	this->displayStatus = Control::SENDING;
+
+	// For receiving always pins are high
+	digitalWrite(25, HIGH);
+	digitalWrite(26, HIGH);
+	digitalWrite(27, HIGH);
 }
 
 void Control::setDisplayReceiving() {
 	this->displayStatus = Control::RECEIVING;
+
+	// For receiving always pins are low
+	digitalWrite(25, LOW);
+	digitalWrite(26, LOW);
+	digitalWrite(27, LOW);
 }
 
 Control::DisplayStatus Control::getDisplayStatus() const {
