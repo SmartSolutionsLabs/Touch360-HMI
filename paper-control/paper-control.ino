c8a17952f970941cfe760a57a3630aa1a40601d3
el-crag <azertyggmf@outlook.com>
2024-04-04 11:28:41 -0500

    Modified main file for making everything executable

--- paper-control/paper-control.ino
+++ paper-control/paper-control.ino
@@ -1,8 +1,18 @@
+#include "Display.hpp"
+
 void setup() {
 	Serial2.begin(115200);
+	pinMode(25, OUTPUT);
+	pinMode(26, OUTPUT);
+	pinMode(27, OUTPUT);
 	Serial.begin(115200);
+
+	Display * display = new Display("hmi");
+	display->start();
 }
 
+TickType_t xDelay = 1 / portTICK_PERIOD_MS;
+
 void loop() {
-	vTaskDelay( 1 / portTICK_PERIOD_MS);
+	vTaskDelay(xDelay);
 }
