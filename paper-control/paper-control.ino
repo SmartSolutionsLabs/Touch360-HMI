6702b1095e7282878ac3072720f9f732cccf038e
el-crag <azertyggmf@outlook.com>
2024-04-19 21:23:42 -0500

    Added interruption function for capturing laser

--- paper-control/paper-control.ino
+++ paper-control/paper-control.ino
@@ -1,11 +1,17 @@
 #include "Display.hpp"
 
+// When laser detects paper cut
+void IRAM_ATTR interruptCutterHandler() {
+}
+
 void setup() {
 	Serial2.begin(115200);
 	pinMode(25, OUTPUT);
 	pinMode(26, OUTPUT);
 	pinMode(27, OUTPUT);
 	Serial.begin(115200);
+
+	attachInterrupt(digitalPinToInterrupt(35), interruptCutterHandler, FALLING);
 
 	Display * display = new Display("hmi");
 	display->start();
