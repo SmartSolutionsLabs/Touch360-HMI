50539de70d4724ba6211b1ae392fe085f928a438
el-crag <azertyggmf@outlook.com>
2024-04-23 17:54:57 -0500

    In config HMI has been separated Serial and Serial2 using macros

--- paper-control/stone_config.h
+++ paper-control/stone_config.h
@@ -26,21 +26,22 @@
 #include "HardwareSerial.h"
 
 // Reference to Serial
-#define SERIAL_OBJECT Serial2
+#define SERIAL_HMI Serial2
+#define SERIAL_DEBUGGER Serial
 
 #define STONE_Delay(num)       delayMicroseconds(num*1000)
-#define stone_uart_get_flag(USER_UART)   SERIAL_OBJECT.available()
-#define stone_uart_read(USER_UART)   (STONE_RX_BUF[STONE_RX_CNT]=SERIAL_OBJECT.read())
+#define stone_uart_get_flag(USER_UART)   SERIAL_HMI.available()
+#define stone_uart_read(USER_UART)   (STONE_RX_BUF[STONE_RX_CNT]=SERIAL_HMI.read())
 
-#define transport_unfinished SERIAL_OBJECT.availableForWrite()
-#define stone_Transmit(BUF, ...); SERIAL_OBJECT.println(("%s",BUF));
+#define transport_unfinished SERIAL_HMI.availableForWrite()
+#define stone_Transmit(BUF, ...); SERIAL_HMI.println(("%s",BUF));
 #define print_with_flash           1//Whether to enable the storage of fixed strings to be printed in Flash (enable saves memory and reduces running speed; disable improves running speed and occupies memory)
-#define stone_print   SERIAL_OBJECT.print
-#define stone_printf(string)     ((print_with_flash) ? SERIAL_OBJECT.print(F(string)) : SERIAL_OBJECT.print(string))
-#define stone_println   SERIAL_OBJECT.println
-#define stone_printfln(string)     ((print_with_flash) ? SERIAL_OBJECT.println(F(string)) : SERIAL_OBJECT.println(string))
-#define stone_write     SERIAL_OBJECT.write
-#define stone_writef(string)     ((print_with_flash) ? SERIAL_OBJECT.write(F(string)) : SERIAL_OBJECT.write(string))
+#define stone_print   SERIAL_DEBUGGER.print
+#define stone_printf(string)     ((print_with_flash) ? SERIAL_DEBUGGER.print(F(string)) : SERIAL_DEBUGGER.print(string))
+#define stone_println   SERIAL_DEBUGGER.println
+#define stone_printfln(string)     ((print_with_flash) ? SERIAL_DEBUGGER.println(F(string)) : SERIAL_DEBUGGER.println(string))
+#define stone_write     SERIAL_DEBUGGER.write
+#define stone_writef(string)     ((print_with_flash) ? SERIAL_DEBUGGER.write(F(string)) : SERIAL_DEBUGGER.write(string))
 
 
 //Print instructions to parse data for Arduino observation******************************************
